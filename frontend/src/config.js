891794d197532e83a9aee46e2122260bf8a4014b
Nitin Rana <nitinrana0529@gmail.com>
2025-07-02 01:16:27 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://caresync-backend-l3a7.onrender.com";
+export const BASE_URL = "https://caresync-backend-l3a7.onrender.com/api/v1";
 
 export const token = localStorage.getItem('token')
